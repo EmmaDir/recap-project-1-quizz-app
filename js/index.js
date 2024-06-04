5c0f27b0099d1f7cf37b3e22ca1c073c5a4de6a3
Emma <emma.dirscherl@gmail.com>
2024-06-04 17:45:20 +0200

    add and structure css files

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
-// const mainElement = document.querySelector('[data-js="main"]');
-
-// const toggleButton = document.querySelector('[data-js="toggle-button"]');
-// toggleButton.addEventListener("click", () => {
-//   mainElement.classList.toggle("dark");
-//   //   console.log("Button pressed");
-// });
-
 const bodyElement = document.querySelector('[data-js="body"]');
 
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
 toggleButton.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
-  //   console.log("Button pressed");
 });
