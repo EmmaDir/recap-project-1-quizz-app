cf31d4a36e9eb5aa32ae24a183554fcff6b8dccd
Emma <emma.dirscherl@gmail.com>
2024-06-12 14:40:56 +0200

    add function to toggle color of bookmarks

--- js/index.js
+++ js/index.js
@@ -34,6 +34,39 @@ answerButton4.addEventListener("click", () => {
   cardAnswer4.classList.toggle("card__answer--show");
 });
 
+// bookmark toggle
+// 1
+const bookmarkButton1 = document.querySelector('[data-js="bookmark__button1"]');
+
+bookmarkButton1.addEventListener("click", () => {
+  bookmarkButton1.style.color =
+    bookmarkButton1.style.color === "orange" ? "lightgray" : "orange";
+});
+
+// 2
+const bookmarkButton2 = document.querySelector('[data-js="bookmark__button2"]');
+
+bookmarkButton2.addEventListener("click", () => {
+  bookmarkButton2.style.color =
+    bookmarkButton2.style.color === "orange" ? "lightgray" : "orange";
+});
+
+// 3
+const bookmarkButton3 = document.querySelector('[data-js="bookmark__button3"]');
+
+bookmarkButton3.addEventListener("click", () => {
+  bookmarkButton3.style.color =
+    bookmarkButton3.style.color === "orange" ? "lightgray" : "orange";
+});
+
+// 4
+const bookmarkButton4 = document.querySelector('[data-js="bookmark__button4"]');
+
+bookmarkButton4.addEventListener("click", () => {
+  bookmarkButton4.style.color =
+    bookmarkButton4.style.color === "orange" ? "lightgray" : "orange";
+});
+
 // dark mode
 
 const bodyElement = document.querySelector('[data-js="body"]');
